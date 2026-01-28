9e210cf27d3eb1d5e9a1edc9268c09959e3e5dec
Yurii <yerikstar@gmail.com>
2026-01-28 15:28:09 +0200

    create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from 'next/link';
+import styles from './page.module.css';
 
 export default function Home() {
-  return <h1>Home Page</h1>
+  return (
+    <main>
+      <section className={styles.hero}>
+        <div className={styles.container}>
+          <h1 className={styles.title}>Campers of your dreams</h1>
+          <p className={styles.subtitle}>
+            You can find everything you want in our catalog
+          </p>
+          <Link href="/catalog" className={styles.button}>
+            View Now
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
 }
